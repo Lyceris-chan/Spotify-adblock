48eb08b75f64186dddba353c952b6f426c2a554b
Lacia~chan <ghostdrain@outlook.com>
2021-04-29 18:40:36 +0000

    Setup: initial script
    This script automagically downloads the latest release using PowerShell invoked in CMD and then extracts and replaces the files needed to remove the ads in Spotify after which it removes the files it downloaded and the user can then enjoy Spotify without ads.

---
+++ Setup.bat
@@ -0,0 +1,43 @@
+@echo off
+
+rem Download the latest SpotifyAdBlocker-x86 release from github using Invoke-WebRequest
+powershell.exe Invoke-WebRequest -Uri https://github.com/moadfakhri/SpotifyAdBlocker-Windows/releases/latest/download/SpotifyAdBlocker-x86.zip -OutFile SpotifyAdBlocker-x86.zip
+
+rem Extract the latest version of SpotifyAdBlocker-x86
+powershell.exe Expand-Archive -Force SpotifyAdBlocker-x86.zip SpotifyAdBlocker-x86
+
+rem Save current directory as a variable
+set originalDir=%cd%
+
+rem Change directory into %appdata%/Spotify
+cd %appdata%/Spotify
+
+rem Rename libcef.dll to libcefNative.dll
+ren libcef.dll libcefNative.dll
+
+rem Go back to the original directory the user executed the script from
+cd %originalDir%
+
+rem Change directory into the folder we executed the latest SpotifyAdBlocker-x86 in
+cd %originalDir%/SpotifyAdBlocker-x86
+
+rem Move the modded libcef.dll to %appdata%/Spotify/
+move libcef.dll %appdata%/Spotify
+
+rem Go back to the %originalDir%
+cd %originalDir%
+
+rem Cleanup temporary files
+del SpotifyAdBlocker-x86.zip
+rmdir SpotifyAdBlocker-x86 /q /s
+
+rem Clear screen of previous commands and their outputs
+cls
+
+rem Print the following message
+echo Enjoy Spotify without Ads.
+
+rem Print an empty line
+echo.
+
+pause
